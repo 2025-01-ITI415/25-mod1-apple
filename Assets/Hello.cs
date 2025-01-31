d566b8504cbaadcc3bc1f50e41275d9d8a06c3be
hyakaera <72956208+hyakaera@users.noreply.github.com>
2025-01-30 19:36:32 -0500

    Apple Picker Object Set Up
    Created Tree, Apple, and Basket

---
+++ Assets/Hello.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(this.gameObject.name + " Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
