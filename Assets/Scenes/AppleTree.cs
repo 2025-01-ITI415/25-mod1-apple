2b7934c0ad714db1f11501cb0147af2f9f5fdd73
devdesai10 <devandesai03@gmail.com>
2025-02-01 08:38:29 -0500

    Finished First Script

--- Assets/Scenes/AppleTree.cs
+++ Assets/Scenes/AppleTree.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+//Prefab for instantiatin the appple
+public GameObject applePrefab;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+//Speed that the Tree moves in m/s
+public float speed = 1f;
+
+//Distance where apple tree turns around
+//I dont understand what this function does is it like the switch ways?
+public float leftAndRightEdge =10f;
+
+//Chance that the tree will change directions
+public float chanceToChangeDirection
+
+//Rate at which the apples will be created
+public float SecondsBetweenAppleDrop
+
+void Start() {
+    //Dropping Apples every second
+}
+
+void Update(){
+    //Basic Movement 
+    //Changing Directions
+}
 }
