8100c3bd44c3ad292d274e7c3ed26d3b75457269
Jonah Sosis <jonahsosis@gmail.com>
2025-02-03 02:09:48 -0500

    got up to page 21
    finished everything up to dropping apples

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    //Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    //Speed at which the AppleTree moves
+    public float speed=1f;
+    //Distance where AppleTree turns Around
+    public float leftAndRightEdge=10f;
+    //Chance that the AppleTree will change directions
+    public float chanceToChangeDirections=0.1f;
+    //Rate at which Apples will be instantiated
+    public float secondsBetweenAppleDrops=1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Dropping apples every second
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        //changing directions
+        if(pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }else if (pos.x > leftAndRightEdge)//Move right
+        {
+            speed = -Mathf.Abs(speed);//move left
+        }
+    }
+    private void FixedUpdate()
+    {
+        {
+            //Changing Direction Randomly is now timed
+            if(Random.value < chanceToChangeDirections)
+            {
+                speed *= -1;//change direction
+            }
+        }
     }
 }
