74f62b7aad08f61ffad2c0e60b42175b8bc94981
mnhabashy <maria.habashy@rutgers.edu>
2025-02-04 18:39:08 -0500

    Catching Apples
    I modified the basket code so the apples would disappear when they collide with it

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -31,4 +31,15 @@ public class Basket : MonoBehaviour
         pos.x = mousePos3D.x;
         this.transform.position = pos;
     }
+
+    void OnCollisionEnter( Collision coll ) {
+// a
+        // Find out what hit this basket
+        GameObject collidedWith = coll.gameObject;
+// b
+        if ( collidedWith.CompareTag("Apple") ) {
+// c
+            Destroy( collidedWith );
+        }
+    }
 }
