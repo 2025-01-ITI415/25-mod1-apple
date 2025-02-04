8e5844a00acc05b83ca2978de23d4b9a6ed591cd
Infinetly <io99@scarletmail.rutgers.edu>
2025-02-03 23:43:57 -0500

    did everything up to highscore but not complete
    The score will not update since I am using the textmesh pro instead of the regular text which I cannot find. either download tmp library and import it to use or make the text normal text

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -4,19 +4,10 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour {
     [Header("Inscribed")]
-    // Prefab for instantiating apples
     public GameObject applePrefab;
-
-    // Speed at which the AppleTree moves
     public float speed = 1f;
-
-    // Distance where AppleTree turns around
     public float leftAndRightEdge = 10f;
-
-    // Chance that the AppleTree will change directions
     public float changeDirChance = 0.1f;
-
-    // Seconds between Apples instantiations
     public float appleDropDelay = 1f;
 
     void Start () {
@@ -31,12 +22,10 @@ public class AppleTree : MonoBehaviour {
 
 
     void Update () {
-        // Basic Movement
         Vector3 pos = transform.position;
         pos.x += speed * Time.deltaTime;
         transform.position = pos;
 
-        // Changing Direction
         if ( pos.x < -leftAndRightEdge ) {
             speed = Mathf.Abs( speed ); // Move right
             } 
@@ -48,7 +37,6 @@ public class AppleTree : MonoBehaviour {
         //}
     }
     void FixedUpdate() {
-        // Random direction changes are now time-based due to
         if ( Random.value < changeDirChance ) {
             speed *= -1; // Change direction
         }
