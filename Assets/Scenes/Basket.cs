183e35c22cec53a9e3c49edcb88c2d4b872d93e6
devdesai10 <devandesai03@gmail.com>
2025-02-05 18:13:39 -0500

    j

--- Assets/Scenes/Basket.cs
+++ Assets/Scenes/Basket.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Basket : MonoBehaviour
 {
-
     // Start is called before the first frame update
     void Start()
     {
@@ -15,18 +14,22 @@ public class Basket : MonoBehaviour
     void Update()
     {
         Vector3 mousePos2D = Input.mousePosition;
+        
         mousePos2D.z = -Camera.main.transform.position.z;
 
         Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
         Vector3 pos = this.transform.position;
-        pos.x mousePos3D.x;
+        pos.x = mousePos3D.x; // Fixed: Added the assignment operator (=)
         this.transform.position = pos;
     }
 
-    void OnCollisionEnter( Collision coll){
-        GameObject collideWith = coll.gameObject;
-        if (colliderWith.CompareTag("Apple")){
-            Destroy(collideWith);
+    void OnCollisionEnter(Collision coll)
+    {
+        // Find out what hit this basket
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.CompareTag("Apple"))
+        {
+            Destroy(collidedWith);
         }
     }
 }
