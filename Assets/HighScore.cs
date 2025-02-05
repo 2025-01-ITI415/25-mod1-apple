842f7617541eff506f0afd90d068a79b391357c5
spm207 <spm207@scarletmail.rutgers.edu>
2025-02-04 21:38:41 -0500

    High Score Preserved
    the high score  is now saved even after quitting and relaunching the game

--- Assets/HighScore.cs
+++ Assets/HighScore.cs
@@ -10,6 +10,12 @@ public class HighScore : MonoBehaviour {
     private Text txtCom;    // txtCom is a reference to this GO's Text component
     void Awake () {
         _UI_TEXT = GetComponent<Text>();
+        // If the PlayerPrefs HighScore already exists, read it
+        if (PlayerPrefs.HasKey("HighScore")) {
+            SCORE = PlayerPrefs.GetInt("HighScore");
+        }
+        // Assign the high score to HighScore
+        PlayerPrefs.SetInt("HighScore", SCORE);
 
     }
 
@@ -17,6 +23,8 @@ public class HighScore : MonoBehaviour {
         get { return _SCORE; }
         private set {
             _SCORE = value;
+            PlayerPrefs.SetInt("HighScore", value);
+
             if ( _UI_TEXT != null ) {
                 _UI_TEXT.text = "High Score: " +
 value.ToString( "#,0" );
@@ -27,5 +35,19 @@ value.ToString( "#,0" );
     static public void TRY_SET_HIGH_SCORE( int scoreToTry ) {
         if ( scoreToTry <= SCORE ) return;  // If scoreToTry is too low, return
         SCORE = scoreToTry;
+    }
+
+    //The following code allows you to easily reset the PlayerPrefs HighScore
+    [Tooltip( "Check this box to reset the HighScore in PlayerPrefs" )]
+    public bool resetHighScoreNow = false;
+    
+    void OnDrawGizmos() {
+        
+        if ( resetHighScoreNow ) {
+            resetHighScoreNow = false;
+            PlayerPrefs.SetInt("Highscore", 1000 );
+            Debug.LogWarning( "PlayerPrefs HighScore reset to 1,000.");
+
+        }
     }
 }
