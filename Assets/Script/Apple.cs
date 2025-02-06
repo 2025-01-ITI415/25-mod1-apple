7c5706e45ff7148b9413048a0ac7eb6981b95ff1
matchamatt <146647075+matchamatt@users.noreply.github.com>
2025-02-06 15:30:36 -0500

    duplicating scene

--- Assets/Script/Apple.cs
+++ Assets/Script/Apple.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Apple : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static float bottomY = -20f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if ( transform.position.y < bottomY ) {
+            Destroy( this.gameObject );
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+            apScript.AppleMissed();
+        }
     }
 }
+
