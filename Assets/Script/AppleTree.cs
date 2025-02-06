766a891846631f8329393bf295273fce4579dda9
matchamatt <146647075+matchamatt@users.noreply.github.com>
2025-02-06 15:06:36 -0500

    adding physics

--- Assets/Script/AppleTree.cs
+++ Assets/Script/AppleTree.cs
@@ -7,17 +7,17 @@ public class AppleTree : MonoBehaviour {
     public GameObject applePrefab;
     public float speed = 1f;
     public float leftAndRightEdge = 10f;
-    public float changeToChangeDirection 
-    public float secondsBetweenAppleDrop
+    public float changeDirChance = 0.1f;
+    public float appleDropDelay = 1f;
 
     void Start () {
-        
+        Invoke( "DropApple", 2f );
     }
     void DropApple() {
         GameObject apple = Instantiate<GameObject>(
         applePrefab ); 
         apple.transform.position = transform.position;
-        Invoke( "DropApple", secondsBetweenAppleDrop );
+        Invoke( "DropApple", appleDropDelay );
     }
 
 
@@ -25,15 +25,20 @@ public class AppleTree : MonoBehaviour {
         Vector3 pos = transform.position;
         pos.x += speed * Time.deltaTime;
         transform.position = pos;
-    if ( pos.x < -leftAndRightEdge ) {
+
+        if ( pos.x < -leftAndRightEdge ) {
             speed = Mathf.Abs( speed ); // Move right
             } 
         else if ( pos.x > leftAndRightEdge ) {
             speed = -Mathf.Abs( speed ); // Move left
         }
-        //else if ( Random.value < changeToChangeDirection ) {
+        //else if ( Random.value < changeDirChance ) {
         //    speed *= -1; // Change 
         //}
     }
-    
+    void FixedUpdate() {
+        if ( Random.value < changeDirChance ) {
+            speed *= -1; // Change direction
+        }
+    }
 }
