8555fef8d579154eb7f41c5c14882e6f13c1b1d9
chairmp4 <ifd8503@gmail.com>
2025-02-05 23:58:40 -0500

    2/5 11:58
    Working on UI ! (its broken... yipeee...)

--- Assets/Apple.cs
+++ Assets/Apple.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Orange : MonoBehaviour
+public class Apple : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    public static float     bottomY = -20f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,12 @@ public class Orange : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if ( transform.position.y < bottomY ) {
+            Destroy (this.gameObject );
         
+        // Get a reference to the ApplePicker
+        
+        } 
     }
 }
+ 
--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class Basket : MonoBehaviour
 {
+    public ScoreCounter scoreCounter;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Find a GameObject named ScoreCounter in the Scene Hierarchy
+        GameObject scoreGO = GameObject.Find( "ScoreCounter" );
+
+        // Get the ScoreCounter (Script) component of scoreGO
+        scoreCounter = scoreGO.GetComponent<ScoreCounter>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Get the current screen psoition of the mouse from Input
+        Vector3 mousePos2D = Input.mousePosition;
+
+        // The Camera's z position sets how far to push the mouse into 3D
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        // Convert the point from 2D screen space into 3D game world space
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint( mousePos2D );
         
+        // Move the z position of this basket to the z position of the Mouse
+        Vector3 pos = this.transform.position;
+        pos.z = mousePos3D.z;
+        this.transform.position = pos;
+    }
+
+    void OnCollisionEnter ( Collision coll ) {
+        // Find out what hit this basket
+        GameObject collidedWith = coll.gameObject;
+
+        if ( collidedWith.CompareTag("Apple") ) {
+            Destroy( collidedWith );
+            // Increase the score
+            scoreCounter.score += 100;
+        }
     }
 }
