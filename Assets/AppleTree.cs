523920115588bdebff970d7a5a7822ea91e49ade
Daniel H <danielmople1@gmail.com>
2025-02-06 20:04:44 -0500

    THE TREE MOVES

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -13,12 +13,27 @@ public class AppleTree : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if ( pos.x < -leftAndRightEdge ) {
+            speed = Mathf.Abs(speed); // Move ri
+        }
+        else if ( pos.x > leftAndRightEdge ) {
+            speed = -Mathf.Abs(speed); // Move l
+        }
+    }
+    void FixedUpdate() {
+        // Changing Direction Randomly is now t
+        if ( Random.value < chanceToChangeDirections ){
+            speed *= -1; // Change direction
+        }
     }
 }
